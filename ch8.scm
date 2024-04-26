dc31f25e261ffb8483b5156d3abe530490b8c8fa
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-26 15:32:19 -0700

    add equal? function

--- ch8.scm
+++ ch8.scm
@@ -1,1 +1,36 @@
 #lang sicp
+
+; returns true if x is an atom
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+; returns true if the two arguments are the same atom
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2))
+       (= a1 a2))
+      ((or (number? a1) (number? a2))
+       #f)
+      (else (eq? a1 a2)))))
+
+; returns true if the two arguments are lists and are equal
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1) (null? l2)) #t)
+      ((or (null? l1) (null? l2)) #f)
+      (else
+       (and (equal? (car l1) (car l2))
+            (eqlist? (cdr l1) (cdr l2)))))))
+
+; returns true if s1 and s2 are the same atom, or if they are the same list. 
+(define equal?
+  (lambda (s1 s2)
+    (cond
+      ((and (atom? s1) (atom? s2))
+       (eqan? s1 s2))
+      ((or (atom? s1) (atom? s2))
+       #f)
+      (else (eqlist? s1 s2)))))
