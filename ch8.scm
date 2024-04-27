fc2bc59ba523e16d8c4f162f6d74f44f1460d4ae
beakerandjake <beakerandjake@users.noreply.github.com>
2024-04-27 11:54:08 -0700

    wip ch8

--- ch8.scm
+++ ch8.scm
@@ -35,13 +35,11 @@
        #f)
       (else (eqlist? s1 s2)))))
 
-
 ; returns a function which compares an x value to the a value
 (define eq?-c
   (lambda (a)
     (lambda (x)
       (eq? x a))))
-
 
 ; returns a rember function which compares elements using the test? function.
 (define rember-f
@@ -53,7 +51,56 @@
         (else (cons (car l)
                     ((rember-f test?) a
                                       (cdr l))))))))
-      
 
-      
-    
+; returns an insertL function which compares elements using the test? function.
+; the insertL function inserts the new value to the left of the first occurrence of the old value
+(define insertL-f
+  (lambda (test?)
+    (lambda (new old l)
+      (cond
+        ((null? l) '())
+        ((test? old (car l))
+         (cons new (cons old (cdr l))))
+        (else (cons (car l)
+                    ((insertL-f test?) new old (cdr l))))))))
+
+; returns an insertR function which compares elements using the test? function.
+; the insertR function inserts the new value to the right of the first occurrence of the old value
+(define insertR-f
+  (lambda (test?)
+    (lambda (new old l)
+      (cond
+        ((null? l) '())
+        ((test? old (car l))
+         (cons old (cons new (cdr l))))
+        (else (cons (car l)
+                    ((insertR-f test?) new old (cdr l))))))))
+
+; conses the first arg onto the result of consing the second arg onto the third arg
+(define seqL
+  (lambda (new old l)
+    (cons new (cons old l))))
+
+; conses the second arg onto the result of consing the first arg onto the third arg
+(define seqR
+  (lambda (new old l)
+    (cons old (cons new l))))
+
+; returns a functinon which uses seq to cons the new value to the first occurance of the old value in l.
+(define insert-g
+  (lambda (seq)
+    (lambda (new old l)
+      (cond
+        ((null? l) '())
+        ((eq? (car l) old)
+         (seq new old (cdr l)))
+        (else (cons (car l)
+                    ((insert-g seq) new old (cdr l))))))))
+
+; inserts the new value to the left of the first occurrence of the old value
+(define insertL
+  (insert-g seqL))
+
+; inserts the new value to the right of the first occurrence of the old value
+(define insertR
+  (insert-g seqR))
